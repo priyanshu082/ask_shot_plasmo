8354d9c0addd710f1edbc6c0a5165af173d01de8
priyanshu082 <priyanshusingh216@gmail.com>
2025-09-13 22:25:57 +0530

    index file compelte

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import { AuthScreen } from "./AuthScreen"
+import { ChatHeader } from "./ChatHeader"
+import { ChatInput } from "./ChatInput"
+import { ChatMessages } from "./ChatMessages"
+import { ParticleBackground } from "./ParticleBackground"
+import { ScreenshotDisplay } from "./ScreenshotDisplay"
+import { ScreenshotHistory } from "./ScreenshotHistory"
+
+export {
+  AuthScreen,
+  ChatHeader,
+  ChatInput,
+  ChatMessages,
+  ParticleBackground,
+  ScreenshotDisplay,
+  ScreenshotHistory
+}
